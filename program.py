67bccade7d3efb026ac7dda89afed496bd2a25c3
Samarth100 <jainsamarth2007@gmail.com>
2021-04-11 10:02:16 +0530

    regitration form

--- program.py
+++ program.py
@@ -1,2 +1,22 @@
-print("samarth")
-print("Hello world")
+nameList=[]
+sectionList=[]
+marksList=[]
+
+while True:
+    print()
+    choice = int(input("Press 1 for adding student record, Press 2 to EXIT "))
+    if choice == 1:
+        name = input("Whats Your Name ")
+        nameList.append(name)
+        section = input("In which Class You are ")
+        sectionList.append(section)
+        marks = input("Whats were your marks ")
+        marksList.append(marks)
+    else:
+        break
+
+print("\n Display records :")
+print(nameList)
+print(sectionList)
+print(marksList)
+      
